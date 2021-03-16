6ea49c8132cdb9139c7aa6b00d4f919aa175f1f6
Hyeonyee <jgh10070@naver.com>
2021-03-16 16:53:00 +0900

    [ADD] Pakage

---
+++ Burmilla/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+
+function App() {
+  <BrowserRouter>
+  <Switch>
+    
+  </Switch>
+  </BrowserRouter>
+}
+
+export default App;
