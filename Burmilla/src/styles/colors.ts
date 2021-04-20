181d3d83ff06edfdac0ff49760cd81cdc2d2b419
Hyeonyee <jgh10070@naver.com>
2021-04-20 22:49:18 +0900

    [UPDATE] styles

---
+++ Burmilla/src/styles/colors.ts
@@ -0,0 +1,7 @@
+export const primaryColor = '#62d3e8';
+export const darkPrimaryColor = '#0f2c4d';
+export const lightPrimaryColor = '#effbfd';
+export const accentColor = '#46b2c6';
+export const primaryTextColor = '#292929';
+export const darkBorderColor = '#909090';
+export const lightBorderColor = '#3f3f3f';
