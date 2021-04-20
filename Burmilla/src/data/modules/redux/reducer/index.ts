f882974de1784b8d44cc4f4eee9822e2c1a73eaf
Hyeonyee <jgh10070@naver.com>
2021-04-20 22:52:29 +0900

    [UPDATE] redux/reducer/index.ts

---
+++ Burmilla/src/data/modules/redux/reducer/index.ts
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+
+// import applicant from './applicant';
+// import auth from './auth';
+// import statistics from './statistics';
+
+const rootReducer = combineReducers({
+    // applicant,
+    // auth,
+    // statistics,
+})
+
+export default rootReducer;
